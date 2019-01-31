d7e1668a5bbe4b32ef25a0a9b7c0a7984c9153fa
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2019-01-31 14:11:11 +0100

    Add a few tests

---
+++ src/Path.spec.ts
@@ -0,0 +1,24 @@
+import { expect } from "chai";
+// tslint:disable-next-line:no-import-side-effect
+import "mocha";
+import { Path } from "./Path";
+
+describe("Path", () => {
+    let goodPath: Path | undefined;
+    let badPath: Path | undefined;
+
+    beforeEach(() => {
+        goodPath = new Path("./LICENSE");
+        badPath = new Path("./234987298374");
+    });
+
+    describe("canAccess", () => {
+        it("should return true", async () => {
+            expect(goodPath && await goodPath.canAccess()).to.equal(true);
+        });
+
+        it("should return false", async () => {
+            expect(badPath && await badPath.canAccess()).to.equal(false);
+        });
+    });
+});
