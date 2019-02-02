cc1309c71290a3ca63a114d03b0744585549d123
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2019-02-02 11:56:12 +0100

    Partially extend for getStats

--- src/Path.spec.ts
+++ src/Path.spec.ts
@@ -5,25 +5,26 @@ import { Path } from "./Path";
 
 type ExpectedArray = [ boolean, boolean, boolean, boolean ];
 type PathArray = [ Path, Path, Path, Path ];
+type Method = "canAccess" | "canExecute" | "getStats";
 
 describe("Path", () => {
-    const checkPermissions = (method: "canAccess" | "canExecute", ...expected: ExpectedArray) => {
-            describe(method, () => {
-                const paths: PathArray = [
-                    new Path(".", "234987298374"),
-                    new Path(".", "LICENSE"),
-                    new Path(".", "publish"),
-                    new Path(".", "src"),
-                ];
+    const checkResult = <T extends Method>(method: T, ...expected: ExpectedArray) => {
+        describe(method, () => {
+            const paths: PathArray = [
+                new Path(".", "234987298374"),
+                new Path(".", "LICENSE"),
+                new Path(".", "publish"),
+                new Path(".", "src"),
+            ];
 
-                for (let index = 0; index < paths.length; ++index) {
-                    it(`should return ${expected[index]}`, async () => {
-                        expect(await paths[index][method]()).to.equal(expected[index]);
-                    });
-                }
-            });
-        };
+            for (let index = 0; index < paths.length; ++index) {
+                it(`should return ${expected[index]} for ${paths[index].path}`, async () => {
+                    expect(!!await paths[index][method]()).to.equal(expected[index]);
+                });
+            }
+        });
+    };
 
-    checkPermissions("canAccess", false, true, true, true);
-    checkPermissions("canExecute", false, false, true, true);
+    checkResult("canAccess", false, true, true, true);
+    checkResult("canExecute", false, false, true, true);
 });
