f8be783055825d917d8e689ca2c297e0755dc3a0
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2019-02-03 09:44:49 +0100

    Add changeMode tests

--- src/Path.spec.ts
+++ src/Path.spec.ts
@@ -6,37 +6,46 @@ import { Path } from "./Path";
 
 type ExpectedArray = [ boolean, boolean, boolean, boolean ];
 type PathArray = [ Path, Path, Path, Path ];
-type Method = "canAccess" | "canExecute" | "getStats";
+type Method = "canAccess" | "canExecute" | "getStats" | "getFiles";
 
 describe("Path", async () => {
-    const checkResult = (method: Method, checker: (path: Path) => Promise<boolean>, ...expected: ExpectedArray) => {
+    const checkResult = (method: Method, checker: (sut: Path) => Promise<boolean>, ...expected: ExpectedArray) => {
         describe(method, () => {
-            const paths: PathArray = [
+            const sut: PathArray = [
                 new Path(".", "234987298374"),
                 new Path(".", "LICENSE"),
                 new Path(".", "publish"),
                 new Path(".", "src"),
             ];
 
-            for (let index = 0; index < paths.length; ++index) {
-                it(`should evaluate to ${expected[index]} for ${paths[index].path}`, async () => {
-                    expect(await checker(paths[index])).to.equal(expected[index]);
+            for (let index = 0; index < sut.length; ++index) {
+                it(`should evaluate to ${expected[index]} for ${sut[index].path}`, async () => {
+                    expect(await checker(sut[index])).to.equal(expected[index]);
                 });
             }
         });
     };
 
-    const getStatsChecker = async (path: Path) => {
+    const getStatsChecker = async (sut: Path) => {
         try {
-            return !!await path.getStats();
+            return !!await sut.getStats();
         } catch (e) {
             return false;
         }
     };
 
-    checkResult("canAccess", (path) => path.canAccess(), false, true, true, true);
-    checkResult("canExecute", (path) => path.canExecute(), false, false, true, true);
+    const getFilesChecker = async (sut: Path) => {
+        try {
+            return !!await sut.getFiles();
+        } catch (e) {
+            return false;
+        }
+    };
+
+    checkResult("canAccess", (sut) => sut.canAccess(), false, true, true, true);
+    checkResult("canExecute", (sut) => sut.canExecute(), false, false, true, true);
     checkResult("getStats", getStatsChecker, false, true, true, true);
+    checkResult("getFiles", getFilesChecker, false, false, false, true);
 
     const testRunPath = new Path("test-run");
 
@@ -47,28 +56,51 @@ describe("Path", async () => {
 
     await testRunPath.createDirectory();
 
+    describe("changeMode", () => {
+        const sut = new Path(testRunPath.path, `${Date.now()}`);
+
+        it ("should fail to change the mode of a missing file", async () => {
+            try {
+                await sut.changeMode(0o777);
+                throw new Error("did not throw as expected");
+            } catch (e) {
+                expect(e instanceof Error && e.message.startsWith("ENOENT: no such file or directory")).to.equal(true);
+            }
+        });
+
+        it("should change the mode of an existing file", async () => {
+            await sut.createDirectory();
+
+            await sut.changeMode(0o000);
+            expect((await sut.getStats()).mode & 0o777).to.equal(0o000);
+            await sut.changeMode(0o777);
+            expect((await sut.getStats()).mode & 0o777).to.equal(0o777);
+        });
+    });
+
     describe("openWrite", () => {
         const end = (stream: WriteStream) => new Promise<void>((resolve) => stream.once("finish", resolve).end());
         const close = (stream: WriteStream) => new Promise<void>((resolve) => stream.once("close", resolve).close());
 
         it("should open a new file for writing", async () => {
-            const path = new Path(testRunPath.path, `${Date.now()}.txt`);
-            const stream = await path.openWrite();
+            const sut = new Path(testRunPath.path, `${Date.now()}.txt`);
+
+            const stream = await sut.openWrite();
             stream.write("Test\n");
             await end(stream);
             await close(stream);
 
-            const stats = await path.getStats();
+            const stats = await sut.getStats();
             expect(stats.isDirectory()).to.equal(false);
             expect(stats.isFile()).to.equal(true);
         });
 
         it("should fail to open a new file for writing", async () => {
-            const path = new Path(testRunPath.path, `${Date.now()}`, `${Date.now()}.txt`);
+            const sut = new Path(testRunPath.path, `${Date.now()}`, `${Date.now()}.txt`);
 
             try {
-                await path.openWrite();
-                throw new Error("openWrite did not throw as expected.");
+                await sut.openWrite();
+                throw new Error("did not throw as expected");
             } catch (e) {
                 expect(e instanceof Error && e.message.startsWith("ENOENT: no such file or directory")).to.equal(true);
             }
