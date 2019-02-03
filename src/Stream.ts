48e6cf8ba2eeacf2a94470f8423f7b31054b6ad3
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2019-02-03 21:36:51 +0100

    Simplify Stream implementation

--- src/Stream.ts
+++ src/Stream.ts
@@ -2,38 +2,26 @@ import { EventEmitter } from "events";
 
 export class Stream {
     public static async create<T extends EventEmitter>(create: () => T): Promise<T> {
-        const factory = new StreamFactory(create);
+        const result = create();
+        let onOpen: (() => void) | undefined;
+        let onError: ((reason?: any) => void) | undefined;
 
         try {
-            return await factory.get();
+            await new Promise<void>((resolve, reject) => {
+                onOpen = resolve;
+                onError = reject;
+                result.on("open", onOpen).on("error", onError);
+            });
+
+            return result;
         } finally {
-            factory.dispose();
+            if (onOpen) {
+                result.removeListener("open", onOpen);
+            }
+
+            if (onError) {
+                result.removeListener("error", onError);
+            }
         }
     }
-}
-
-// tslint:disable-next-line:max-classes-per-file
-class StreamFactory<T extends EventEmitter> {
-    public constructor(create: () => T) {
-        this.promise = new Promise<T>((resolve, reject) => {
-            this.stream = create();
-            this.onOpen = () => resolve(this.stream);
-            this.onError = reject;
-            this.stream.on("open", this.onOpen).on("error", this.onError);
-        });
-    }
-
-    public get() { return this.promise; }
-
-    public dispose() {
-        (this.stream as T).removeListener("open", this.onOpen as (fd: number) => void);
-        (this.stream as T).removeListener("error", this.onError as (err: Error) => void);
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    private stream: T | undefined;
-    private onOpen: ((fd: number) => void) | undefined;
-    private onError: ((err: Error) => void) | undefined;
-    private readonly promise: Promise<T>;
 }
