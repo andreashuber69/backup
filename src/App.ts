f2ae6a9e9aab3c6fc6cb5bd4a73c557208ba41d8
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-12-03 14:41:54 +0100

    refactor: move functions out of the class

--- src/App.ts
+++ src/App.ts
@@ -20,12 +20,62 @@ enum DayOfWeek {
     Saturday,
 }
 
+const getTodayMilliseconds = () => {
+    // We want to get the number of full days between start and today. The current timezone should be considered
+    // such that when the clock moves past midnight in the current timezone then the number of days between start
+    // and today should increase by one. We achieve that by getting the current local year, month and day and then
+    // pretend that they are actually UTC numbers. Likewise, we construct the start with Date.UTC.
+    const now = new Date();
+
+    return Date.UTC(now.getFullYear(), now.getMonth(), now.getDate());
+};
+
+const getMediumName = ({ cacheNumber, slotNumber, serialNumber }: Medium) => {
+    const serial = String.fromCharCode("a".charCodeAt(0) + serialNumber);
+
+    return `${DayOfWeek[(slotNumber + 1) % 7]}${(cacheNumber + 1)}${serial}`;
+};
+
+const getConsoleInput = async () =>
+    await new Promise<string>((resolve) => {
+        const stdin = process.openStdin();
+        stdin.once("data", (args: { readonly toString: () => string }) => {
+            resolve(args.toString().trim());
+            stdin.pause();
+        });
+    });
+
+const requestInput = async (prompt: string) => {
+    process.stdout.write(prompt);
+
+    return await getConsoleInput();
+};
+
+const isExecError = (error: Error | null): error is IExecError => (error ? "code" in error : false);
+
+const getResult = (error: Error | null, stdout: string, stderr: string): IExecResult =>
+    ({
+        output: stdout + stderr,
+        exitCode: isExecError(error) ? error.code : 0,
+        exitMessage: error ? error.toString() : "",
+    });
+
+const execAsync = async (command: string) =>
+    await new Promise<IExecResult>(
+        (resolve) => exec(command, (error, stdout, stderr) => resolve(getResult(error, stdout, stderr))),
+    );
+
+const delay = async (milliseconds: number) => {
+    await new Promise<void>((resolve) => setTimeout(resolve, milliseconds));
+};
+
 class App {
     public static async main() {
-        const todayMilliseconds = App.getTodayMilliseconds();
-        const daysSinceStart = (todayMilliseconds - App.startMilliseconds) / 24 / 60 / 60 / 1000;
+        const startMilliseconds = Date.UTC(2000, 3, 10);
+        const todayMilliseconds = getTodayMilliseconds();
+        const daysSinceStart = (todayMilliseconds - startMilliseconds) / 24 / 60 / 60 / 1000;
         const medium = Medium.get(2, 7, daysSinceStart);
-        const mediumName = App.getMediumName(medium);
+        const mediumName = getMediumName(medium);
         // cSpell: ignore logname
         const user = process.env["LOGNAME"];
         const mediumRoot = new Path("/", "media", user ? user : "", mediumName);
@@ -37,20 +87,20 @@ class App {
             // eslint-disable-next-line no-await-in-loop
             while (!await mediumRoot.canAccess() || !(await mediumRoot.getStats()).isDirectory()) {
                 // eslint-disable-next-line no-await-in-loop
-                await App.requestInput(`Please insert ${mediumName} and press Enter: `);
+                await requestInput(`Please insert ${mediumName} and press Enter: `);
             }
 
             const files =
                 (await mediumRoot.getFiles()).filter((p) => !p.path.endsWith("lost+found"));
             const prompt = "Non-empty medium! Delete everything? [Y/n]: ";
 
-            if ((files.length === 0) || (await App.requestInput(prompt)).toLowerCase() !== "n") {
+            if ((files.length === 0) || (await requestInput(prompt)).toLowerCase() !== "n") {
                 await Promise.all(files.map(async (file) => await file.delete()));
                 const backupScript = new Path(__dirname, "backup");
                 const commandLine = `${backupScript.path} ${mediumRoot.path}`;
-                const resultPromise = App.exec(commandLine);
+                const resultPromise = execAsync(commandLine);
                 // Allow the external process to start and execute past the empty directory check.
-                await App.delay(1000);
+                await delay(1000);
                 logger = await Logger.create(new Path(mediumRoot.path, "log.txt"));
                 logger.writeOutputMarker("Backup Start");
                 logger.writeMediumInfo(new Date(todayMilliseconds), medium, mediumName);
@@ -81,64 +131,6 @@ class App {
                 await logger.dispose();
             }
         }
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    private static readonly startMilliseconds = Date.UTC(2000, 3, 10);
-
-    private static getTodayMilliseconds() {
-        // We want to get the number of full days between start and today. The current timezone should be considered
-        // such that when the clock moves past midnight in the current timezone then the number of days between start
-        // and today should increase by one. We achieve that by getting the current local year, month and day and then
-        // pretend that they are actually UTC numbers. Likewise, we construct the start with Date.UTC.
-        const now = new Date();
-
-        return Date.UTC(now.getFullYear(), now.getMonth(), now.getDate());
-    }
-
-    private static getMediumName({ cacheNumber, slotNumber, serialNumber }: Medium) {
-        const serial = String.fromCharCode("a".charCodeAt(0) + serialNumber);
-
-        return `${DayOfWeek[(slotNumber + 1) % 7]}${(cacheNumber + 1)}${serial}`;
-    }
-
-    private static async requestInput(prompt: string) {
-        process.stdout.write(prompt);
-
-        return await App.getConsoleInput();
-    }
-
-    private static async exec(command: string) {
-        return await new Promise<IExecResult>(
-            (resolve) => exec(command, (error, stdout, stderr) => resolve(App.getResult(error, stdout, stderr))),
-        );
-    }
-
-    private static getResult(error: Error | null, stdout: string, stderr: string): IExecResult {
-        return {
-            output: stdout + stderr,
-            exitCode: App.isExecError(error) ? error.code : 0,
-            exitMessage: error ? error.toString() : "",
-        };
-    }
-
-    private static isExecError(error: Error | null): error is IExecError {
-        return error ? "code" in error : false;
-    }
-
-    private static async delay(milliseconds: number) {
-        await new Promise<void>((resolve) => setTimeout(resolve, milliseconds));
-    }
-
-    private static async getConsoleInput() {
-        return await new Promise<string>((resolve) => {
-            const stdin = process.openStdin();
-            stdin.once("data", (args: { readonly toString: () => string }) => {
-                resolve(args.toString().trim());
-                stdin.pause();
-            });
-        });
     }
 }
 
