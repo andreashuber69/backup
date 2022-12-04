9e4794bc91430350f14336ae64a29b68e1876a0f
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-12-04 09:52:50 +0100

    refactor: update @andreashuber69/eslint-config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,4 @@ module.exports = {
     extends: [
         "@andreashuber69",
     ],
-    rules: {
-        // Would make sense if var declarations were allowed (to avoid different behavior in and outside of a loop).
-        // Since var declarations are not allowed, we can safely turn this off.
-        "init-declarations": "off",
-        "@typescript-eslint/init-declarations": "off",
-    },
 };
