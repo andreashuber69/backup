e9af8a547efd314b4f5e3fcf68f495eabb048e38
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-12-05 17:43:45 +0100

    refactor: extract function

--- src/Medium.spec.ts
+++ src/Medium.spec.ts
@@ -2,54 +2,39 @@ import { expect } from "chai";
 
 import { Medium } from "./Medium.js";
 
-const createMedium = (backupCount: number) => {
-    const slotNames = ["One"] as const;
+const check = (slotNames: readonly string[], expectedMediumNames: readonly string[]) => {
+    for (let index = 0; index < expectedMediumNames.length; ++index) {
+        const medium = new Medium(slotNames, 1, index);
+        const { name, backupCountSinceMediumStart, backupCountUntilMediumEnd } = medium;
 
-    return new Medium(slotNames, 1, backupCount);
-};
-
-const checkProperties = (medium: Medium, expectedName: string) => {
-    const { name, backupCountSinceMediumStart, backupCountUntilMediumEnd } = medium;
-    expect(name).to.equal(expectedName);
-    expect(backupCountSinceMediumStart).to.equal(0);
-    expect(backupCountUntilMediumEnd).to.equal(0);
+        it(name, () => {
+            expect(name).to.equal(expectedMediumNames[index]);
+            expect(backupCountSinceMediumStart).to.equal(0);
+            expect(backupCountUntilMediumEnd).to.equal(0);
+        });
+    }
 };
 
 describe("Medium", () => {
-    const expectedNames = [
-        "One1a",
-        "One1b",
-        "One1c",
-        "One1d",
-        "One1e",
-        "One1f",
-        "One1g",
-        "One1h",
-        "One1i",
-        "One1j",
-        "One1k",
-        "One1l",
-        "One1m",
-        "One1n",
-        "One1o",
-        "One1p",
-        "One1q",
-        "One1r",
-        "One1s",
-        "One1t",
-        "One1u",
-        "One1v",
-        "One1w",
-        "One1x",
-        "One1y",
-        "One1z",
-    ] as const;
+    const slotNames = ["One"] as const;
 
-    for (let index = 0; index < expectedNames.length; ++index) {
-        const medium = createMedium(index);
+    describe(`with slot names ${slotNames.join(", ")}`, () => {
+        const expectedMediumNames = [
+            "One1a",
+            "One1b",
+            "One1c",
+            "One1d",
+            "One1e",
+            "One1f",
+            "One1g",
+            "One1h",
+            "One1i",
+            "One1j",
+            "One1k",
+            "One1l",
+            "One1m",
+        ] as const;
 
-        it(medium.name, () => {
-            checkProperties(medium, expectedNames[index] ?? "");
-        });
-    }
+        check(slotNames, expectedMediumNames);
+    });
 });
