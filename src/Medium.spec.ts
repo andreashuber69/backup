9a041e5e9a8302ca8cdd39b8f46e88a8ec40b97d
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-12-05 14:59:10 +0100

    test: Medium.name

--- src/Medium.spec.ts
+++ src/Medium.spec.ts
@@ -2,12 +2,54 @@ import { expect } from "chai";
 
 import { Medium } from "./Medium.js";
 
-const slotNames = ["One", "Two", "Three"] as const;
+const createMedium = (backupCount: number) => {
+    const slotNames = ["One"] as const;
+
+    return new Medium(slotNames, 1, backupCount);
+};
+
+const checkProperties = (medium: Medium, expectedName: string) => {
+    const { name, backupCountSinceMediumStart, backupCountUntilMediumEnd } = medium;
+    expect(name).to.equal(expectedName);
+    expect(backupCountSinceMediumStart).to.equal(0);
+    expect(backupCountUntilMediumEnd).to.equal(0);
+};
 
 describe("Medium", () => {
-    describe("get", () => {
-        it("should return a Medium instance", () => {
-            expect(new Medium(slotNames, 2, 0) instanceof Medium).to.equal(true);
+    const expectedNames = [
+        "One1a",
+        "One1b",
+        "One1c",
+        "One1d",
+        "One1e",
+        "One1f",
+        "One1g",
+        "One1h",
+        "One1i",
+        "One1j",
+        "One1k",
+        "One1l",
+        "One1m",
+        "One1n",
+        "One1o",
+        "One1p",
+        "One1q",
+        "One1r",
+        "One1s",
+        "One1t",
+        "One1u",
+        "One1v",
+        "One1w",
+        "One1x",
+        "One1y",
+        "One1z",
+    ] as const;
+
+    for (let index = 0; index < expectedNames.length; ++index) {
+        const medium = createMedium(index);
+
+        it(medium.name, () => {
+            checkProperties(medium, expectedNames[index] ?? "");
         });
-    });
+    }
 });
