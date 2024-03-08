3cb14c6286e13c29590e4a13b890cc3efd859838
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-03-08 15:23:28 +0100

    chore: reconfigure linting to include all files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,9 @@
 // https://github.com/andreashuber69/backup/blob/master/README.md#----backup
+// eslint-disable-next-line import/no-commonjs, import/unambiguous
 module.exports = {
     env: {
         node: true,
     },
     extends: ["@andreashuber69"],
+    ignorePatterns: ["/coverage/", "/test-run/"],
 };
